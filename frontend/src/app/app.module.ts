79170cf7854f5cd3b7876b2d86d2efa67f61f748
ValCMoi <valentin.rouxel.pro@gmail.com>
2023-04-28 16:07:07 +0200

    page static

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroBannerComponent } from './components/hero-banner/hero-banner.component';
+import { PropertyCardComponent } from './components/property-card/property-card.component';
+import { ListPropertyCardComponent } from './components/list-property-card/list-property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroBannerComponent,
+    PropertyCardComponent,
+    ListPropertyCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/list-property-card/list-property-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'list-property-card',
+  templateUrl: './list-property-card.component.html',
+  styleUrls: ['./list-property-card.component.css']
+})
+export class ListPropertyCardComponent {
+
+}
